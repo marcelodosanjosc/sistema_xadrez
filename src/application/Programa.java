be2e0559423d02f0dd8c73d9d59ad01318c96e77
marcelodosanjosc <marcelodosanjosc@gmail.com>
2021-03-14 11:42:29 -0400

    Inicio da implementação do tabuleiro e peça

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ola mundo");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
---
+++ src/boardgame/Tabuleiro.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private Peca[][] peca;
+	
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		peca = new Peca[linhas][colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	
+}
